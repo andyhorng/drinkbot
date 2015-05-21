8b9fc5d711d23413287ecbe45bd0cdf1a1f63499
洪盟翔 <andy@eztable.com.tw>
2015-05-21 22:37:03 +0800

    Use simpler state model

--- damnit.py
+++ damnit.py
@@ -4,9 +4,13 @@ import time
 import yaml
 import json
 import threading
-from fysom import Fysom
 
 from slackclient import SlackClient
+
+
+class Input(object):
+    def __init__(self, slack_event):
+        self.slack_event = slack_event
 
 
 class Bot(object):
@@ -15,31 +19,67 @@ class Bot(object):
         self.config = config
         self.slack = SlackClient(config['token'])
         self.menu = json.load(open('./menu.json'))
+        self.state = 'standby'
+        self.shops = self.get_shops()
+        self.shop_id = None
+        self.orders = {}
 
-        self.state = Fysom({
-            'initial': 'standby',
-            'events': [
-                {'name': 'start_to_order',
-                    'src': 'standby', 'dst': 'ordering'},
-                {'name': 'order_drink', 'src': 'ordering', 'dst': 'ordering'},
-                {'name': 'done', 'src': 'ordering', 'dst': 'summary'},
-                {'name': 'confirm', 'src': 'summary', 'dst': 'standby'},
-                ],
-            'callbacks': {
-                'onordering': self.when_ordering,
-                'onorder_drink': self.when_order_drink
-                },
-            })
+    def get_shops(self):
+        i = 0
+        shops = {}
+        for name in dir(self.menu):
+            i += 1
+            shops[i] = name
 
-    def when_ordering(self, e):
-        event = e.args[0]
-        channel = self.slack.server.channels.find(event['channel'])
-        channel.send_message('菜單')
+        return shops
 
-    def when_order_drink(self, e):
-        event = e.args[0]
-        channel = self.slack.server.channels.find(event['channel'])
-        channel.send_message('OK')
+    # states #
+    def state_standby(self, event):
+        if '我要喝飲料' in event['text'].encode('utf-8'):
+            channel = self.slack.server.channels.find(event['channel'])
+            channel.send_message('''
+            好，請輸入飲料店 ID，
+            或輸入list來列出所有飲料店。
+            或直接輸入您的訂單編號。
+            {}
+            '''.format(','.join("{}: {}\n".format(k, v)
+                                for k, v in self.get_shops().items())))
+            return 'choice_shop'
+        return 'standby'
+
+    def state_choice_shop(self, event):
+        text = event['text'].encode('utf-8')
+        try:
+            if int(text) in self.shops.keys():
+                self.shop_id = int(text)
+                return 'confirm_choice_shop'
+        except:
+            return 'choice_shop'
+
+        return 'choice_shop'
+
+    def state_confirm_choice_shop(self, event):
+        text = event['text'].encode('utf-8')
+        try:
+            if int(text) == 1:
+                return 'order_drink'
+            else:
+                return 'choice_shop'
+        except:
+            return 'choice_shop'
+
+    def state_order_drink(self, event):
+        self.orders = {}
+        # dm everybody
+        return 'order_drink'
+
+    # states #
+
+    def interact(self, e):
+        logging.info("state: {} with {}".format(self.state, e))
+        next_state = getattr(self, "state_{}".format(self.state))(e)
+        logging.info("state becomes: {}".format(next_state))
+        return next_state
 
     def up(self):
         connected = self.slack.rtm_connect()
@@ -64,8 +104,8 @@ class Bot(object):
             except Exception as e:
                 logging.info(e)
 
-    # def handle_hello(self, event):
-    #     print event
+    def handle_hello(self, event):
+        print event
 
     def handle_message(self, event):
         # get channel name
@@ -73,17 +113,10 @@ class Bot(object):
         logging.info(channel.name)
 
         if channel.name == self.config['command_channel']:
-            if '我要喝飲料' in event['text'].encode('utf-8'):
-                self.state.start_to_order(event)
-            if '紅茶' in event['text'].encode('utf-8'):
-                self.state.order_drink(event)
+            self.state = self.interact(event)
 
     def log(self, event):
         logging.info(event)
-        logging.info('state: ' + self.state.current)
-
-    def onstart_to_order():
-        pass
 
 
 class Main(object):
